eca66585835a53b1b71b42d31d8d89377a6d3223
tangciwei <tangciwei@baidu.com>
2017-08-04 23:34:01 +0800

    ok

--- analyse/watch.js
+++ analyse/watch.js
@@ -15,39 +15,54 @@ let yunpian = require('./yunpian-sms');
 let log = require('../log');
 let config = require('../config');
 module.exports = {
-    async oneTime() {
-        async function requestOnce({name, web}) {
-            try {
-                await request.common({name, web});
-            }
-            catch (e) {
-                log.trace(name, '接口挂掉了');
-                // await yunpian.post({
-                //     mobile: '18500909025',
-                //     name,
-                //     content: '接口挂掉了'
-                // });
-            }
+    async requestOnce({name, web}) {
+        try {
+            await request.common({name, web});
         }
+        catch (e) {
+            log.trace(name, '接口挂掉了');
+            // await yunpian.post({
+            //     mobile: '18500909025',
+            //     name,
+            //     content: '接口挂掉了'
+            // });
+        }
+    },
+    async quickRequest() {
         await Promise.all([
-            requestOnce({name: 'jubi', web: jubi}),
-            requestOnce({name: 'bter', web: bter}),
-            requestOnce({name: 'yunbi', web: yunbi}),
-            requestOnce({name: 'btc38', web: btc38}),
-            requestOnce({name: 'chbtc', web: chbtc}),
-            requestOnce({name: 'huobi', web: huobi}),
-            requestOnce({name: 'yuanbao', web: yuanbao}),
-            requestOnce({name: 'okcoin', web: okcoin}),
-            requestOnce({name: 'binance', web: binance}),
-            requestOnce({name: 'btc9', web: btc9})
+            this.requestOnce({name: 'jubi', web: jubi}),
+            this.requestOnce({name: 'bter', web: bter}),
+            this.requestOnce({name: 'yunbi', web: yunbi}),
+            this.requestOnce({name: 'btc38', web: btc38}),
+            this.requestOnce({name: 'chbtc', web: chbtc}),
+            this.requestOnce({name: 'huobi', web: huobi}),
+            this.requestOnce({name: 'yuanbao', web: yuanbao}),
+            this.requestOnce({name: 'binance', web: binance})
         ]);
     },
+    // 以下接口请求频率慢点
+    async slowRequest() {
+        await this.requestOnce({name: 'okcoin', web: okcoin});
+        // await Promise.all([
+        //     this.requestOnce({name: 'okcoin', web: okcoin}),
+        //     // this.requestOnce({name: 'btc9', web: btc9})
+        // ]);
+    },
     async init() {
-        await this.oneTime();
-        let time = config.loopTime;
+        await this.quickRequest();
+        await this.slowRequest();
+
         log.trace('第一次成功');
+
+        let time = config.loopTime;
+
         setInterval(() => {
-            this.oneTime();
+            this.quickRequest();
         }, time);
+
+        // 三分钟请求一次
+        setInterval(() => {
+            this.slowRequest();
+        }, 180 * 1000);
     }
 };
--- websites/btc9.js
+++ websites/btc9.js
@@ -10,8 +10,9 @@ module.exports = {
         let html = await util.get({
             url,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-                'Cookie': 'PHPSESSID=fu0ukpahvgvrjn5rg83ige3ll7; UM_distinctid=15dad56a4e4402-0aa016bfc0cb2-30667808-13c680-15dad56a4e562c; CNZZDATA1258868758=921587905-1501847318-%7C1501847318'
+                'Connection': 'keep-alive',
+                'User-Agent': 'Mozilla/4.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+                'Cookie': 'PHPSESSID=58ou7ekqma56dmjnptggkd6824; UM_distinctid=15dadda0c331df-0735f735f11b29-30667808-13c680-15dadda0c349ba; CNZZDATA1258868758=1150427937-1501855660-https%253A%252F%252Fbtc9.com%252F%7C1501855660'
             }
         });
         let $ = cheerio.load(html, {
